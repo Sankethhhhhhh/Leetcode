bbdf45500f885f7813edb5de9a7409c00aa47760
Sanketh S <sanketh0731@gmail.com>
2026-07-04 19:02:28 +0530

    Time: 187 ms (43.74%), Space: 68.9 MB (83.3%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -0,0 +1,27 @@
+from collections import deque
+
+class Solution:
+    def minScore(self, n: int, roads: List[List[int]]) -> int:
+        graph = [[] for _ in range(n + 1)]
+
+        for u, v, w in roads:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
+
+        visited = [False] * (n + 1)
+        q = deque([1])
+        visited[1] = True
+
+        ans = float('inf')
+
+        while q:
+            node = q.popleft()
+
+            for nei, wt in graph[node]:
+                ans = min(ans, wt)
+
+                if not visited[nei]:
+                    visited[nei] = True
+                    q.append(nei)
+
+        return ans
